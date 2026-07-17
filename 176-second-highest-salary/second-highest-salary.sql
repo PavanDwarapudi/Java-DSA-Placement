697c6630a557993cac6d224332519e46202a92d6
PavanDwarapudi <pavandwarapudi1@gmail.com>
2026-07-17 15:49:25 +0530

    Time: 261 ms (94.33%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -6,3 +6,7 @@ ORDER BY salary DESC
 LIMIT 1 OFFSET 1),
 NULL
 ) AS SecondHighestSalary;
+
+
+-- While returning the 2nd Highest One, there can be null values, duplicate values
+-- For that use IFNULL for null values & DISTINCT for duplicate values
